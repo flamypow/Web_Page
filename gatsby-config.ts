f0771cd1d85cba7250e7ea132850a0ab01cf6170
William <65571126+flamypow@users.noreply.github.com>
2024-01-21 20:56:24 -0800

    Update gatsby-config.ts
    small edits

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,9 +9,9 @@ const config: GatsbyConfig = {
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-emilia-core/gatsby-config.mjs
     siteTitle: `Emilia`,
-    siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
-    siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://emilia.lekoarts.de`,
+    siteTitleAlt: `Gatsby Starter Portfolio`,
+    siteHeadline: `Gatsby Theme from @lekoarts`,
+    siteUrl: `https://melodious-flan-007a54.netlify.app/`,
     siteDescription: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI. Includes Light/Dark mode.`,
     siteImage: `/banner.jpg`,
     siteLanguage: `en`,
